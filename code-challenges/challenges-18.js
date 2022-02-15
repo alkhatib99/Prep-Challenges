e39fc28f085c6521718c3bd001de3e9f2e97737a
Alkhatib99 <aboodjob@gmail.com>
2022-02-15 14:25:38 +0200

    exercise18

---
+++ code-challenges/challenges-18.js
@@ -0,0 +1,114 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    // write your code here
+let arr = str.split(' ');
+let middle = parseInt(arr.length/2);
+return arr[middle].length
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes two strings and checks if the two strings contain the same combination of letters
+//
+// Ex:
+// Input: "dad", "add"
+// Output: true
+//
+// Input: "dad", "aadd"
+// Output: false
+//
+// Input: "table", "label"
+// Output: false
+//
+// Input: "cat", "rat"
+// Output: false
+
+const wordLetters = (str1, str2) => {
+    // write your code here
+let array = str1.split('');
+console.log(array)
+array.forEach(element=>{
+    if(str2.includes(element))
+    {
+        str2=str2.replace(element,'');
+    }
+    else {
+        return false;
+    }
+});
+console.log(str2)
+
+if(str2 === "")
+return true;
+else 
+return false
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes an integer and a sorted array
+// The function returns the index of the integer in the array
+// If the integer was not there, the function returns where it could have been
+//
+// Ex:
+// Input: [1,3,5,6], 5
+// Output: 2
+//
+// Input: [1,3,5,6], 2
+// Output: 1
+//
+// Input: [1,3,5,6], 7
+// Output: 4
+//
+
+const targetIndex = (arr, int) => {
+    // write your code here
+
+    if (arr.includes(int))
+    return arr.indexOf(int);
+    else 
+{
+    let    max = Math.max.apply(null, arr);
+if(int > max)
+{
+return arr.length;
+}
+
+else {
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+if(element < int )
+continue;
+else 
+return i;
+
+}
+
+}
+}
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { wordLength, wordLetters, targetIndex };
